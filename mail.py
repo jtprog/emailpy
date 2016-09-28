c8a8af0f0344f94acbd1c8fb6209bf9257a3468e
Mihail Savin <mail@jtprog.ru>
2016-09-28 13:04:16 +0300

    Add class

--- mail.py
+++ mail.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python3.5
 import sys
 from smtplib import SMTP
-from email.mime.text import MIMEText
+from imaplib import 
+
+# Класс пользователя
+class MailUser:
+    login = input('Enter your Gmail account: ')
+    password = input('Enter your Gmail password: ')
+
+
+# Класс сервера
+class MailServer:
+    address = 'smtp.gmail.com'
+    port = '587'
+
+
+# Создаем объект класса Пользователя
+user = MailUser()
+# Создаем объект класса Сервер
+srv = MailServer()
 
 destination = []
-serverSMTP = 'smtp.gmail.com'
-serverPORT = '587'
+# serverSMTP = 'smtp.gmail.com'
+# serverPORT = '587'
 
 # Вводим свой логин на Gmail
 # Чтобы это сработало на вашем аккаунте Gmail
 # необходимо разрешить небезопасный доступ в настройках аккаунта
-sender = input('Enter your Gmail account: ')
+# sender = input('Enter your Gmail account: ')
 # Вводим свой пароль от Gmail
-password = input('Enter your Gmail password: ')
+# password = input('Enter your Gmail password: ')
 
 # Вводим адрес получателя
 destination.append(input('Enter the recepients address: '))
 # Вводим тему
 subject = input('Enter the subject of email: ')
 
-UNAME = sender
-UPASS = password
+UNAME = user.login
+UPASS = user.password
 
 text_subtype = 'plain'
 
@@ -28,8 +45,8 @@ text_subtype = 'plain'
 print("Enter message, end with ^D (Unix) or ^Z (Windows):")
 
 try:
-    msg = ("From: %s\r\nTo: %s\r\n\r\nSubject: %s\r\n"
-           % (sender, ", ".join(destination)))
+    msg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n"
+           % (UNAME, ", ".join(destination), subject))
     while True:
         try:
             line = input()
@@ -37,12 +54,12 @@ try:
             break
         if not line:
             break
-        msg = msg + line
+        msg = msg + line + "\n"
 
     print("Message length is", len(msg))
 
     # Подключение к хосту Gmail
-    connection = SMTP(host=serverSMTP, port=serverPORT)
+    connection = SMTP(host=srv.address, port=srv.port)
     connection.set_debuglevel(False)
     # Приветствие TLS
     connection.ehlo()
@@ -52,7 +69,7 @@ try:
     connection.login(UNAME, UPASS)
     try:
         # Отправляем сообщение
-        connection.sendmail(sender, destination, msg)
+        connection.sendmail(UNAME, destination, msg)
     finally:
         # Отключаемся от сервера
         connection.quit()
